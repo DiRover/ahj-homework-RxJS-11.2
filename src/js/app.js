6f588c58da515b5b6ac961631c921de25d3ccede
Dmitriy Rovenskiy <arhitec@bk.ru>
2021-05-08 13:41:18 +0300

    Heroku server added

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,14 @@ import { forkJoin } from 'rxjs';
 import renderPosts from './renderPosts';
 
 const container = document.querySelector('.container');// создаём основной контейнер
-const url = 'http://localhost:7070/posts/latest';//
+const url = 'https://unionserver.herokuapp.com/posts/latest';
+// const url = 'http://localhost:7070/posts/latest';//
 const request$ = ajax.getJSON(url);// создаём поток с запросом для получения постов
 
 request$.pipe(
   pluck('posts'), // вылавливаем в потоке по ключу массив с постами
   concatMap((posts) => { // последовательно переключаем поток
-    const comments$ = posts.map((post) => ajax.getJSON(`http://localhost:7070/posts/${post.id}/comments/latest`).pipe(// создаём новый потом для получения коментариев к определённому посту
+    const comments$ = posts.map((post) => ajax.getJSON(`https://unionserver.herokuapp.com/posts/${post.id}/comments/latest`).pipe(// создаём новый потом для получения коментариев к определённому посту
       pluck('comments'), // вылавливаем в потоке по ключу массив с коментариями
       map((comments) => {
         const postsWithComments = {};// создаём объект с постом и коментариями к нему
